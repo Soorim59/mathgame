ab59c740a66463965624133f34b92d12ce9a5651
Soorim <laissillva59@gmail.com>
2026-09-18 04:26:32 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
+  plugins: [react()],
+  base: '/mathgame/'
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
